c28d0855a9736d1a19eafdb9fc45ad162d6f2d39
Diego Quintanilla <diego.quintanilla@proton.me>
2023-07-09 17:53:24 -0400

    add custom drop down directive

--- src/app/shared/drop-down.directive.ts
+++ src/app/shared/drop-down.directive.ts
@@ -1,9 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropDown]'
 })
 export class DropDownDirective {
+
+  @HostListener('class.open') isOpen: boolean = false;
+
+  @HostListener('click') toggleOpen(): void {
+    this.isOpen = !this.isOpen;
+  }
 
   constructor() { }
 
